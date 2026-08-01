775df1fcfc01a5734d01e916b3fc3864010952e7
Zahra Ali <zahraali6789qureshi@gmail.com>
2026-08-01 22:49:33 +0500

    Delete Week-4 MARDI Platform directory

--- Week-4 MARDI Platform/src/components/Stat.jsx
+++
@@ -1,8 +0,0 @@
-export default function Stat({ label, value }) {
-  return (
-    <div className="stat-card">
-      <div className="stat-value">{value}</div>
-      <div className="stat-label">{label}</div>
-    </div>
-  );
-}
